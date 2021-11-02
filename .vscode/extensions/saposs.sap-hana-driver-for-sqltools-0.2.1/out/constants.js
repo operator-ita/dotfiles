e3544512f90292b6ddcd55f77d6c55e04d595024
operator-ita <54598359+operator-ita@users.noreply.github.com>
2021-11-02 15:47:50 -0600

    Delete .vscode directory

--- .vscode/extensions/saposs.sap-hana-driver-for-sqltools-0.2.1/out/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DRIVER_ALIASES = void 0;
-exports.DRIVER_ALIASES = [
-    { displayName: 'SAPHana', value: 'SAPHana' },
-];
